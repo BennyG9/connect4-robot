be23016ffde8959e03e0bfc8162b29993670da2d
Benny Goldstein <bennygoldstein99@gmail.com>
2026-04-01 19:21:03 -0400

    type bug fix take 2

--- src/Connect4AI.h
+++ src/Connect4AI.h
@@ -11,23 +11,24 @@ public:
 
     static const int ROWS = 6;
     static const int COLS = 7;
-    int ROW_MASKS[] = {0,0,0,0,0,0,0};
-    int COL_MASKS[] = {0,0,0,0,0,0,0};
     vector<int> W = {100000000,1000,1000,10000,100000,5,10,15,20,15,10,5,5,5,10,10,15,15,-100000000,-1000,-1000,-10000,-100000,-5,-10,-15,-20,-15,-10,-5,-5,-5,-10,-10,-15,-15};
 
     Connect4AI();
 
     bool move_available(int c, int player, int opponent);
-    int get_move(int c, int player, int opponent);
-    vector<int> moves_available(int player, int opponent);
-    bool check_win(int player);
-    int num_wins(int player);
-    int wins(int player);
-    void print_board(int player, int opponent);
-    int model_query(int player, int opponent, vector<int> weights);
+    uint64_t get_move(int c, uint64_t player, uint64_t opponent);
+    vector<int> moves_available(uint64_t player, uint64_t opponent);
+    bool check_win(uint64_t player);
+    int num_wins(uint64_t player);
+    uint64_t wins(uint64_t player);
+    void print_board(uint64_t player, uint64_t opponent);
+    int model_query(uint64_t player, uint64_t opponent, vector<int> weights);
     int user_query();
-    vector<int> fill_feat_vec(int player, int opponent);
-    vector<double> minimax(int player, int opponent, vector<int> weights, int depth, double alpha=-numeric_limits<double>::infinity(), double beta=numeric_limits<double>::infinity(), bool root=true);
+    vector<int> fill_feat_vec(uint64_t player, uint64_t opponent);
+    vector<double> minimax(uint64_t player, uint64_t opponent, vector<int> weights, int depth, double alpha=-numeric_limits<double>::infinity(), double beta=numeric_limits<double>::infinity(), bool root=true);
+
+    int ROW_MASKS[] = {0,0,0,0,0,0,0};
+    int COL_MASKS[] = {0,0,0,0,0,0,0};
 
 };
 
